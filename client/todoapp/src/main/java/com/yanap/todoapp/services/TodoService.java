2da801cb1aedb685c0408a6a5845cd61f9a1cd23
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-22 05:37:57 +0900

    サービスクラス定義

---
+++ client/todoapp/src/main/java/com/yanap/todoapp/services/TodoService.java
@@ -0,0 +1,22 @@
+package com.yanap.todoapp.services;
+
+import java.util.List;
+
+import com.yanap.todoapp.models.Todo;
+import com.yanap.todoapp.repositories.TodoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+// TODOサービス
+@Service
+public class TodoService {
+    // リポジトリ
+    @Autowired
+    private TodoRepository repository;
+
+    // ユーザＩＤから列挙
+    public List<Todo> collectByUserId(int userId) {
+        return repository.findByUserId(userId);
+    }
+}
