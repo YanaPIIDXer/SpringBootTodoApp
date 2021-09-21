bd13a19f43510e39e564e274ae51d9107a56d61f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-22 01:42:01 +0900

    登録ページをとりあえず作成し、トップページと相互にリンクを張った

--- client/todoapp/src/main/java/com/yanap/todoapp/IndexController.java
+++ client/todoapp/src/main/java/com/yanap/todoapp/IndexController.java
@@ -5,8 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    // トップページ
     @RequestMapping("/")
     public String Index() {
         return "index";
-    }    
+    }
+
+    // 登録ページ
+    // @TODO:後でUserControllerでも定義してそっちに移す
+    @RequestMapping("/user/register")
+    public String Register() {
+        return "user/register";
+    }
 }
