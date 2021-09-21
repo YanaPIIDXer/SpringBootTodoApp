4265b374c76f0decdb2aa1b2ee23982a4b4a82d7
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-22 02:17:22 +0900

    ユーザモデルの中身

--- client/todoapp/src/main/java/com/yanap/todoapp/models/User.java
+++ client/todoapp/src/main/java/com/yanap/todoapp/models/User.java
@@ -1,5 +1,27 @@
 package com.yanap.todoapp.models;
 
-public class User {
-    
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    @Id
+    @Column
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
 }
