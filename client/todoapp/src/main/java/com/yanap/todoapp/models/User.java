5cfd7fa3f5d6c50a905927c71e5e7f3675c19776
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-22 02:18:05 +0900

    テーブル名を指定

--- client/todoapp/src/main/java/com/yanap/todoapp/models/User.java
+++ client/todoapp/src/main/java/com/yanap/todoapp/models/User.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User implements Serializable {
     @Id
     @Column
