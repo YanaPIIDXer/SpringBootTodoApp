c501a67101ecfb4717f23ed1e5d5394e9e83f0c8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-22 03:36:54 +0900

    ユーザ登録処理のHTML部分

--- client/todoapp/src/main/java/com/yanap/todoapp/requests/UserRequest.java
+++ client/todoapp/src/main/java/com/yanap/todoapp/requests/UserRequest.java
@@ -6,9 +6,6 @@ import javax.validation.constraints.NotEmpty;
 
 import com.yanap.todoapp.models.User;
 
-import lombok.Data;
-
-@Data
 public class UserRequest implements Serializable {
     @NotEmpty(message = "名前を入力してください")
     private String name;
@@ -16,9 +13,15 @@ public class UserRequest implements Serializable {
     @NotEmpty(message = "パスワードを入力してください")
     private String password;
 
+    // @HACK:lombokが仕事しないので自前でアクセッサ定義
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
     // ユーザを生成
     public User createUser() {
-        // @TODO:パスワードは暗号化する
+        // @TODO:パスワードは暗号化する 
         return new User(name, password);
     }
 }
