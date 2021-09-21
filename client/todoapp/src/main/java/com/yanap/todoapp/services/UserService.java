7675f909e5ef96a72e46c49000851e8ddd370013
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-22 02:23:51 +0900

    ユーザサービスクラス定義

---
+++ client/todoapp/src/main/java/com/yanap/todoapp/services/UserService.java
@@ -0,0 +1,21 @@
+package com.yanap.todoapp.services;
+
+import com.yanap.todoapp.models.User;
+import com.yanap.todoapp.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    // リポジトリ
+    @Autowired
+    private UserRepository repository;
+
+    // 保存
+    public boolean save(User user) {
+        return (repository.save(user) != null);
+    }
+    
+}
