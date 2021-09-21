6a398f884b2177c152e3b75b100ee7eebb42ec08
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-22 01:25:28 +0900

    リネーム

--- client/todoapp/src/main/java/com/yanap/IndexController.java
+++ client/todoapp/src/main/java/com/yanap/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ActionController {
+public class IndexController {
     @RequestMapping("/")
     public String Index() {
         return "index";
