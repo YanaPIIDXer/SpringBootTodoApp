66d7bc08d803f54287f03649169a9c28186eb847
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-22 01:42:48 +0900

    コントローラ用のパッケージを追加

--- client/todoapp/src/main/java/com/yanap/todoapp/controllers/IndexController.java
+++ client/todoapp/src/main/java/com/yanap/todoapp/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.yanap.todoapp;
+package com.yanap.todoapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
