a250e036a430abbcffe4b2c549e3c60c5b9cf722
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-22 05:17:38 +0900

    ログアウト実装

--- client/todoapp/src/main/java/com/yanap/todoapp/SecurityConfig.java
+++ client/todoapp/src/main/java/com/yanap/todoapp/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 // セキュリティ設定
 @Configuration
@@ -41,6 +42,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // それ以外は認証が必要
         http.authorizeRequests()
             .anyRequest().authenticated();
+
+        // ログアウト
+        http.logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+            .logoutUrl("/user/logout")
+            .logoutSuccessUrl("/");
     }
     
     // 認証設定
