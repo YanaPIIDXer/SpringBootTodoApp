05bc4f992b18592c4825c1b87e1765e2471299a9
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-22 06:49:23 +0900

    更新処理を見越してアクセッサ追加

--- client/todoapp/src/main/java/com/yanap/todoapp/models/Todo.java
+++ client/todoapp/src/main/java/com/yanap/todoapp/models/Todo.java
@@ -41,8 +41,11 @@ public class Todo {
         this.priority = priority;
     }
     
-    // 必要になるアクセッサ
+    // 各種アクセッサ
     public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
     public String getBody() { return body; }
+    public void setBody(String body) { this.body = body; }
     public int getPriority() { return priority; }
+    public void setPriority(int priority) { this.priority = priority; }
 }
