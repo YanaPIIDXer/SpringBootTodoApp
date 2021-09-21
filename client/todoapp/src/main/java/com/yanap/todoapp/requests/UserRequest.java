3a60a1e487a98f621ea87fb1da8223331a7bd20d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-22 04:38:13 +0900

    不要になったメソッドも撤去

--- client/todoapp/src/main/java/com/yanap/todoapp/requests/UserRequest.java
+++ client/todoapp/src/main/java/com/yanap/todoapp/requests/UserRequest.java
@@ -1,10 +1,7 @@
 package com.yanap.todoapp.requests;
 
 import java.io.Serializable;
-
 import javax.validation.constraints.NotEmpty;
-
-import com.yanap.todoapp.models.User;
 
 // ユーザ新規登録・ログイン用リクエストデータ
 public class UserRequest implements Serializable {
@@ -22,9 +19,4 @@ public class UserRequest implements Serializable {
     public void setName(String name) { this.name = name; }
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-
-    // ユーザを生成
-    public User createUser() {
-        return new User(name, password);
-    }
 }
